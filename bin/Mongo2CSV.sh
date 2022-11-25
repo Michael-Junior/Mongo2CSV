1e9412f756537c98f8f84d60e9d700d11073e386
michael-junior <michaeljunioroliveira@gmail.com>
2022-11-25 13:43:29 -0300

    Changes gitignore and msg out

--- bin/Mongo2CSV.sh
+++ bin/Mongo2CSV.sh
@@ -2,7 +2,7 @@
 
 cd /home/oliveirmic/Projetos-dev/Mongo2CSV/scala/ || exit
 
-sbt "runMain m2csv.Mongo2CSV $1 $2 $3 $4 $5 $6 $7"
+sbt "runMain mongo2csv.Mongo2CSV $1 $2 $3 $4 $5 $6 $7"
 ret="$?"
 
 cd - || exit
