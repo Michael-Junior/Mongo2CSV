e123a73e6ffd098d13c9dfad3db4105a96aa63ed
michael-junior <michaeljunioroliveira@gmail.com>
2022-11-25 05:41:46 -0300

    Method fixes and deprecated call updates

--- src/main/scala/mongo2csv/MongoExport.scala
+++ src/main/scala/mongo2csv/MongoExport.scala
@@ -1,7 +1,6 @@
 package m2csv
 
 import org.mongodb.scala._
-import org.mongodb.scala.bson.collection.mutable.Document
 
 import java.util.concurrent.TimeUnit
 import scala.concurrent.Await
@@ -29,29 +28,30 @@ class MongoExport(database: String,
   private val dbase: MongoDatabase = mongoClient.getDatabase(database)
   private val coll: MongoCollection[Document] = dbase.getCollection(collection)
 
-  val documentsMongo: Seq[Document] = new DocumentObservable[String](coll.find()).observable.results()
+  def documentsMongo: Seq[Document] = new DocumentObservable(coll.find()).observable.results()
 
-  implicit class DocumentObservable[C](val observable: Observable[Document]) extends ImplicitObservable[Document] {
+  implicit class DocumentObservable(val observable: Observable[Document]) extends ImplicitObservable[Document] {
     override val converter: Document => String = doc => doc.toJson()
   }
 
   implicit class GenericObservable[C](val observable: Observable[C]) extends ImplicitObservable[C] {
-    override val converter: (C) => String = (doc) => Option(doc).map(_.toString).getOrElse("")
+    override val converter: C => String = doc => Option(doc).map(_.toString).getOrElse("")
   }
 
   trait ImplicitObservable[C] {
     val observable: Observable[C]
-    val converter: (C) => String
+    val converter: C => String
 
     def results(): Seq[C] = Await.result(observable.toFuture(), Duration(120, TimeUnit.SECONDS))
 
-    def headResult() = Await.result(observable.head(), Duration(10, TimeUnit.SECONDS))
+    //    def headResult(): C = Await.result(observable.head(), Duration(10, TimeUnit.SECONDS))
 
-    def printResults(initial: String = ""): Unit = {
-      if (initial.nonEmpty) print(initial)
-      results().foreach(res => println(converter(res)))
-    }
+    //    def printResults(initial: String = ""): Unit = {
+    //      if (initial.nonEmpty) print(initial)
+    //      results().foreach(res => println(converter(res)))
+    //    }
 
-    def printHeadResult(initial: String = ""): Unit = println(s"${initial}${converter(headResult())}")
+    //    def printHeadResult(initial: String = ""): Unit = println(s"${initial}${converter(headResult())}")
+    //  }
   }
 }
